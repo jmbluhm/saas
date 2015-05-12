cae7f3ef3f6ae17d880fa4b8ee5d09e0a034841f
Jordan Bluhm <Jmbluhm@gmail.com>
2015-05-12 07:31:19 +0000

    Added contact form verification and submission notifications

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,19 @@ class ContactsController < ApplicationController
   end
   
   def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:success] = "Your message was sent. We will get back to you promptly!"
+      redirect_to new_contact_path 
+    else
+      flash[:danger] = 'An error occured; your message was not sent. Maybe give it another try.'
+      redirect_to new_contact_path
+    end
   end
+  
+  private
+    def contact_params
+       params.require(:contact).permit(:name, :email, :comments)
+    end
+  
 end
